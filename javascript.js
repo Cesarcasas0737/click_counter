fffc2e0b57dc0cebf2abf4d8dd0cdc11d681e18a
cesar <cesarcasas0737@gmail.com>
2022-10-16 12:42:41 -0700

    Changes colors based if neg or pos

--- javascript.js
+++ javascript.js
@@ -1,11 +1,13 @@
 var clicks = 0;
 
 const counter = document.querySelector('#counter');
+
+//Operators
 const add = document. querySelector('#add');
 const subtract = document.querySelector('#subtract');
 const reset = document.querySelector('#reset');
 
-
+//Functions
 function onAdd(){
     clicks += 1;
     counter.innerHTML = clicks;
@@ -22,18 +24,33 @@ function onReset(){
     clicks = 0;
     counter.innerHTML = clicks;
     console.log(clicks);
+    
+}
+
+function checkValue(){
+    if (clicks >0){
+        document.body.style.backgroundColor = "lightgreen";
+    }else if(clicks == 0 ){
+        document.body.style.backgroundColor = "white";
+    }else{
+        document.body.style.backgroundColor = "maroon";
+    }
 }
 
 counter.innerHTML = clicks;
+
 add.addEventListener('click',() => {
     onAdd();
+    checkValue();
 });
 
 subtract.addEventListener('click',() => {
     onSub();
+    checkValue();
 });
 
 reset.addEventListener('click',() => {
     onReset();
+    checkValue();
 });
 
